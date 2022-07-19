dab20c29fac368064ef1a4cc92a6f006459cb913
Svetoslav <speednet@mail.bg>
2022-07-19 12:57:15 +0300

    EntityRelations

---
+++ EntityRelations/P01_StudentSystem/P01_StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Win32.SafeHandles;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+
+        [Column(TypeName = "varchar(max)")]
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+
+    }
+}
---
+++ EntityRelations/P01_StudentSystem/P01_StudentSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new StudentSystemContext();
+        }
+    }
+}
